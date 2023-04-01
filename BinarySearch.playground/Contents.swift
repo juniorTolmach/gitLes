9d2d25f88839b74359ade70b69346d57bcce8692
Daniil Oreshenkov <tolmachbum@mail.ru>
2023-04-01 21:19:02 +1000

    Add BinarySearch

---
+++ BinarySearch.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+func binarySearch(list: [Int], item: Int) -> Any{
+    
+    var low = 0
+    var high = list.count - 1
+//    var mid = (low + high) / 2
+    
+    while low <= high {
+        var mid = (low + high) / 2
+
+        var guess = list[mid]
+        if guess == item {
+            return "\(mid + 1)"
+        }
+        if guess > item {
+            high = mid - 1
+
+        } else {
+            low = mid + 1
+        }
+    }
+    
+    return "Error"
+}
+
+let list = [1, 2, 3, 5, 6, 7, 8, 9]
+
+let result = binarySearch(list: list, item: 9)
+
+print(result)
